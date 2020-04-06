c2242d68a44452e22829dbff80ef6d075130cbfe
Hubert Lipinski <hubert.lipinski@gmail.com>
2020-04-06 21:59:16 +0200

    Added description to PrimeFactors kata

--- src/test/kotlin/io/github/hlipinski/PrimeFactorsTest.kt
+++ src/test/kotlin/io/github/hlipinski/PrimeFactorsTest.kt
@@ -5,6 +5,20 @@ import org.junit.jupiter.api.Test
 
 class PrimeFactorsTest {
 
+    /*
+    1 => []
+    2 => [2]
+    3 => [3]
+    4 => [2, 2]
+    5 => [5]
+    6 => [2, 3]
+    7 => [7]
+    8 => [2, 2, 2]
+    9 => [3, 3]
+    ...
+    4620 => [2, 2, 3, 5, 7, 11]
+     */
+
     private val factors = PrimeFactors()
 
     @Test
